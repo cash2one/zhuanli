78f79f5ff048838a175c2e60a34e30251779c24c
zhangdongmao <deanraccoon@gmail.com>
2011-05-11 16:22:32 +0800

    add xml file

--- zhuanliUpload/test.py
+++ zhuanliUpload/test.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python
 #coding: utf8
-import dom,xml
-LOGINURL=
-ADDINFOURL=
+from xml.dom import minidom
+
+class PatentParser():
+    elements=['title','application_number','tech_name','patent_type','problem_solved','patent_solution','keywords','patent_usage_field','patent_pic1file','patent_pic2file']
+    def __init__(self,xml_file):
+        """
+        """
+        self.xml_file=xml_file
+    def getall(self):
+        one={}
+        xmldoc=minidom.parse(self.xml_file)
+        patent=xmldoc.firstChild
+        for i in PatentParser.elements:
+               one[i]=patent.getElementsByTagName(i)[0].firstChild.data.strip()
+        return one
+        
+
+LOGINURL=""
+ADDINFOURL=""
+if __name__=='__main__':
+    x=PatentParser('patent.xml')
+    for k,v in x.getall().items():
+        print k,v

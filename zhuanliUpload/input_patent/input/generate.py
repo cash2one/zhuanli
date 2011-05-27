f523fa83bc76dbceac037354311adb0e7f2afa86
zhangdongmao <deanraccoon@gmail.com>
2011-05-27 18:15:47 +0800

    start write the ouput app

--- zhuanliUpload/input_patent/input/generate.py
+++ zhuanliUpload/input_patent/input/generate.py
@@ -3,6 +3,6 @@ sys.path.append("../..")
 sys.path.append("..")
 from input.models import *
 from datetime import datetime
+lastExcutetime=ExcuteTime.objects.get(pk=1)
 now=datetime.now()
-lastUpate=
-Patent.objects.filter(updateTime_gte=datetime.now())
+patents=Patent.objects.filter(updateTime__gt=lastExcutetime)
